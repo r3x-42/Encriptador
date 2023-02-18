addab2db70699c698caeca8a8c012437725809bc
Andru <andruvargas023@gmail.com>
2023-02-18 12:49:57 -0600

    mejorando desencriptar

--- src/main.js
+++ src/main.js
@@ -16,31 +16,10 @@ let botonDesencriptador = document.querySelector("#botonDesencriptador");
 botonEncriptador.addEventListener('click', encriptar) // CallBack
 
 // ListenerButonDesencriptador
-botonDesencriptador.addEventListener('click', desencriptarBtn) // CallBack
+botonDesencriptador.addEventListener('click', desencriptar) // CallBack
 
 // ListenerButonCopiar
 btnCopiar.addEventListener('click', copiar)
-
-// Funcion validar
-function esValido(mensaje){
-
-    let respuesta = true;
-    let ascii;
-    const textoDescompuesto = descomponerTexto(mensaje);
-
-    // validar mayúsculas y carácteres especiales
-    for(let i = 0; i < textoDescompuesto.length; i++){
-        if(textoDescompuesto[i] != " "){
-            if(textoDescompuesto[i] === textoDescompuesto[i].toUpperCase())
-                respuesta = false;
-            ascii = textoDescompuesto[i].charCodeAt(0);
-            if(ascii < 64 && ascii > 91)
-                respuesta = false;
-        }
-    }
-
-    return respuesta;
-    }
 
 // Funcion Encriptar
  function encriptar() {
@@ -58,28 +37,17 @@ function esValido(mensaje){
  
 
 // Funcion DesencriptarTexto
-function desencriptar(textoDesencriptado){
-    let matriz = [["enter","e"], ["imes","i"], ["ai","a"], ["ober","o"], ["ufat","u"]];
-
-    textoDesencriptado = textoDesencriptado.toLowerCase();
-
-    for(let i = 0; i < matriz.length; i++){
-
-        if(textoDesencriptado.includes(matriz[i][0])){
-
-            textoDesencriptado = textoDesencriptado.replaceAll(matriz[i][0],
-                matriz[i][1])
-        }
-    }
-    return textoDesencriptado;
+function desencriptar(){
+  let mensaje = document.querySelector('#mensaje').value;
+  let textoDescifrado = mensaje
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "");
+    let mensajeFinal = document.querySelector('#mensajeFinal');
+    mensajeFinal.innerHTML = textoDescifrado
 } 
-
-// Funcion desencriptarBtn
-function desencriptarBtn(){
-    const mensajeDesencriptado = desencriptar(mensaje.value)
-    mensajeFinal.innerHTML = mensajeDesencriptado
-    mensaje.value = ""
-}
 
 // Funcion copiar  
 function copiar(){
